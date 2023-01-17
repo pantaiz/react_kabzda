502d6945bd9f3746c64cf16485537076f23ef983
Pantaiz <pantaiz@yandex.ru>
2023-01-18 00:25:51 +0300

    02wipusk

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            This is APP component
+            <Raiting/>
+        </div>
+    );
+}
+
+function Raiting() {
+    return (
+        <div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+        </div>
+    )
+}
+
+function Accordion() {
+    debugger
+    return (<div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
