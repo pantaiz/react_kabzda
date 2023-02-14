dc1690fbc2665cf1bf2f016a0e11fbbd05cd42ed
Pantaiz <pantaiz@yandex.ru>
2023-02-14 16:00:16 +0300

    Рефакторил акардион по нажатию на х3

--- src/component/SelfControlAccordion/SelfAccordionTitle.tsx
+++ src/component/SelfControlAccordion/SelfAccordionTitle.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 type AccordionTitlePropstype = {
     head: string
+    setCompresion:(a:boolean)=>void
+    compresion:boolean
 }
 
 export function AccordionTitle(props: AccordionTitlePropstype) {
-    return <h3> {props.head}
+    return <h3 onClick={()=>{props.setCompresion(!props.compresion)}}> {props.head}
         Menu</h3>
 }
--- src/component/SelfControlAccordion/SelfControlAccordion.tsx
+++ src/component/SelfControlAccordion/SelfControlAccordion.tsx
@@ -9,17 +9,11 @@ type AccordionPropsType = {
 
 
 const SelfControlAccordion = (props: AccordionPropsType) => {
-    const [compresion,setCompresion]=useState(true)
+    const [compresion, setCompresion] = useState(true)
     return (
         <>
-            <button onClick={()=>{setCompresion(!compresion)}}>{compresion?<>Cкукожить</>:<>Разкукожить</>}</button>
-            {compresion
-            ?
-            <>
-                <AccordionTitle head={props.title}/><AccordionBody body={props.title}/>
-            </>
-            :
-            <AccordionTitle head={props.title}/>}
+            <AccordionTitle compresion={compresion} setCompresion={setCompresion} head={props.title}/>
+            {compresion && <AccordionBody body={props.title}/>}
         </>
     )
 }
